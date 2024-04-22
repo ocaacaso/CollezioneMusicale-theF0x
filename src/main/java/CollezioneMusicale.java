a2658df069627d74c6c06666fb1fba62fff5529d
ocaacaso <153017262+ocaacaso@users.noreply.github.com>
2024-04-22 19:02:41 +0000

    i

--- src/main/java/CollezioneMusicale.java
+++ src/main/java/CollezioneMusicale.java
@@ -3,13 +3,19 @@ import java.util.ArrayList;
 public class CollezioneMusicale {
   ArrayList<Supporto> supporti = new ArrayList<Supporto>();
 
-  public void aggiungiSupporto (Supporto supporto) {
+  public void aggiungiSupporto(Supporto supporto) {
     supporti.add(supporto);
   }
 
-  public void rimuoviSupporto (Supporto supporto) {
+  public void rimuoviSupporto(Supporto supporto) {
     supporti.remove(supporto);
   }
 
-  public 
+  public ArrayList<Canzone> cercaCanzonePerAutore(String autore) {
+    ArrayList<Canzone> result = new ArrayList<Canzone>();
+    for (Supporto s : supporti) {
+      result.addAll(s.cercaCanzonePerAutore(autore));
+    }
+    return result;
+  }
 }
